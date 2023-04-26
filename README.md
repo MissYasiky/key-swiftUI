ebfdd3d57fb482bb88ac1fe8781cf9a55d525342
MissYasiky <gzxieyunjia@gmail.com>
2023-04-26 16:37:45 +0800

    创建空项目

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # key-swiftUI
+
 XYJKey的SwiftUI版本
---
+++ key-swiftUI/key_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  key_swiftUIApp.swift
+//  key-swiftUI
+//
+//  Created by MissYasiky on 2023/4/26.
+//
+
+import SwiftUI
+
+@main
+struct key_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
